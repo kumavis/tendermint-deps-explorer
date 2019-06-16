9287be0c55203fcbba28ef19e15811e753a30064
kumavis <aaron@kumavis.me>
2019-06-16 23:21:04 +0800

    cleanup

--- src/graphs/DepGraph.js
+++ src/graphs/DepGraph.js
@@ -54,14 +54,6 @@ class DepGraph extends React.Component {
 
 module.exports = DepGraph
 
-function labelForFileSize (size) {
-  const fileSizeOrder = Math.floor((Math.log(size)/Math.log(10))/3)
-  const fileSizeUnit = ['b','kb','mb'][fileSizeOrder]
-  const fileSizeForUnit = size / Math.pow(10, fileSizeOrder * 3)
-  const fileSizeForUnitFormatted = (size > 1000) ? fileSizeForUnit.toFixed(1) : fileSizeForUnit
-  const fileSizeLabel = `${fileSizeForUnitFormatted} ${fileSizeUnit}`
-  return fileSizeLabel
-}
 
 function createGraphByMode (bundleData, mode) {
   // create graph for mode
@@ -108,20 +100,11 @@ function createModuleGraph (bundleData) {
   // for each module, create node and links 
   Object.keys(bundleData).forEach(parentId => {
     const { file, package:packageName, deps, size, entry } = bundleData[parentId]
-    // const scale = 1 / 20
-    // const radius = scale * Math.sqrt(size)
     const radius = 5
-    // const radius = 5
-    // const fileSizeLabel = labelForFileSize(size)
-    // const label = `${fileSizeLabel} ${packageName}\n${file}`
     const configForPackage = configData.resources[packageName] || {}
     const configLabel = JSON.stringify(configForPackage, null, 2)
     const label = `${packageName}\n${file}\n${configLabel}`
     const isEntryPackage = packageName === '<root>'
-    // entry module is orange
-    // entry pacakge (app code) is blue
-    // deps are green
-    // let color = entry ? 'orange' : (isEntryPackage ? 'blue' : 'green')
     const color = isEntryPackage ? 'purple' : getColorForConfig(configForPackage)
     // create node for modules
     nodes.push(
